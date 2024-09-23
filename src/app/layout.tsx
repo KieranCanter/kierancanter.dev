445e9c6325a9afd4f715131b96736d979c63eb8d
Kieran <canter.kieran@gmail.com>
2024-09-23 00:06:17 -0400

    Removed unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.scss";
-import { ReactNode } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
