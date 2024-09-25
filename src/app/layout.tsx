86d43e6336a38f275d383130405312aaf97a7d52
Kieran <canter.kieran@gmail.com>
2024-09-25 14:44:54 -0400

    Removed head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
