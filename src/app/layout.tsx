6fbef4cd2fcf217005e26c89d07e211c67ce99ce
Kieran <canter.kieran@gmail.com>
2024-09-27 00:32:46 -0400

    Redefined favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./variables.scss";
 
 export const metadata: Metadata = {
   title: "kierancanter.dev",
-  description: "If you're reading this, drink milk",
+  description: "If you're reading this, drink more milk",
 };
 
 export default function RootLayout({
@@ -20,25 +20,29 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-status-bar-style" content="auto" />
         
         {/* Standard favicons */}
-        <link rel="icon" type="image/x-icon" href="../assets/favicons/ICO/logo-light.ico" />
-        <link rel="icon" type="image/x-icon" href="../assets/favicons/ICO/logo-dark.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="../assets/favicons/PNG/logo-light_16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="../assets/favicons/PNG/logo-light_32.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="../assets/favicons/PNG/logo-light_512.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="../assets/favicons/PNG/logo-dark_16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="../assets/favicons/PNG/logo-dark_32.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="../assets/favicons/PNG/logo-dark_512.png" />
+        <link rel="icon" type="image/x-icon" href="/assets/favicons/ICO/logo-light.ico" />
+        <link rel="icon" type="image/x-icon" href="/assets/favicons/ICO/logo-dark.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/PNG/logo-light_16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/PNG/logo-light_32.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/assets/favicons/PNG/logo-light_512.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/PNG/logo-dark_16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/PNG/logo-dark_32.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/assets/favicons/PNG/logo-dark_512.png" />
         
+        {/* Android Icon */}
+        <link rel="icon" sizes="192x192" href="/assets/favicons/PNG/logo-light_android.png" />
+        <link rel="icon" sizes="192x192" href="/assets/favicons/PNG/logo-dark_android.png" />
+
         {/* Apple Touch Icon */}
-        <link rel="apple-touch-icon" sizes="180x180" href="../assets/favicons/PNG/logo-light_apple.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="../assets/favicons/PNG/logo-dark_apple.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/PNG/logo-light_apple.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/PNG/logo-dark_apple.png" />
         
         {/* SVG favicon for modern browsers */}
-        <link rel="icon" type="image/svg+xml" href="../assets/favicons/SVG/logo-light.svg" />
-        <link rel="icon" type="image/svg+xml" href="../assets/favicons/SVG/logo-dark.svg" />
+        <link rel="icon" type="image/svg+xml" href="/assets/favicons/SVG/logo-light.svg" />
+        <link rel="icon" type="image/svg+xml" href="/assets/favicons/SVG/logo-dark.svg" />
         
         {/* Web App Manifest */}
-        <link rel="manifest" href="site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest" />
 
       </head>
       <body className="antialiased">
