53cda0b2234146e9f21df3cd205bcdeb269f73d5
Kieran <canter.kieran@gmail.com>
2024-09-26 23:00:53 -0400

    Navigation bar theme matches system preference

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
-        <meta name="theme-color" content="var(--bg)" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="var(--bg)" />
+        <meta name="theme-color" content="auto" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="auto" />
       </head>
       <body className="antialiased">
         {children}
