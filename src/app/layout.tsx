56cb6016a0a88b5c2690389cffebad97e4223366
Kieran <canter.kieran@gmail.com>
2024-09-27 00:11:31 -0400

    Moved assets to public folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,20 +20,25 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-status-bar-style" content="auto" />
         
         {/* Standard favicons */}
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/favicon-512x512.png" />
+        <link rel="icon" type="image/x-icon" href="../assets/favicons/ICO/logo-light.ico" />
+        <link rel="icon" type="image/x-icon" href="../assets/favicons/ICO/logo-dark.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="../assets/favicons/PNG/logo-light_16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="../assets/favicons/PNG/logo-light_32.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="../assets/favicons/PNG/logo-light_512.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="../assets/favicons/PNG/logo-dark_16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="../assets/favicons/PNG/logo-dark_32.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="../assets/favicons/PNG/logo-dark_512.png" />
         
         {/* Apple Touch Icon */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="../assets/favicons/PNG/logo-light_apple.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="../assets/favicons/PNG/logo-dark_apple.png" />
         
-        {/* Optional: SVG favicon for modern browsers */}
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        {/* SVG favicon for modern browsers */}
+        <link rel="icon" type="image/svg+xml" href="../assets/favicons/SVG/logo-light.svg" />
+        <link rel="icon" type="image/svg+xml" href="../assets/favicons/SVG/logo-dark.svg" />
         
-        {/* Optional: Web App Manifest */}
-        <link rel="manifest" href="/site.webmanifest" />
+        {/* Web App Manifest */}
+        <link rel="manifest" href="site.webmanifest" />
 
       </head>
       <body className="antialiased">
