e38d212c7105f92e48536883892eae6a0344e2b3
Kieran <canter.kieran@gmail.com>
2024-09-27 02:41:30 -0400

    Moved viewport items outside of metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.scss";
 import "./variables.scss";
 
@@ -29,12 +29,13 @@ export const metadata: Metadata = {
     locale: "en_US",
     type: "website",
   },
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
-  },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: "auto",
 };
 
