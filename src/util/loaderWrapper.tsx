3f6eea4e5250cafed5926e2c259709f317a45508
Kieran <canter.kieran@gmail.com>
2024-09-29 00:54:54 -0400

    Ensuring loader runs on mobile devices

--- src/util/loaderWrapper.tsx
+++ src/util/loaderWrapper.tsx
@@ -1,26 +1,23 @@
 import React, { useState, useEffect, useContext } from 'react';
-import dynamic from 'next/dynamic';
+import LogoLoader from '@/components/loader';
 import Home from '@/app/home';
 import '@/styles/variables.scss';
 import { ThemeContext } from '@/context/themeContext';
-
-// Dynamically import LogoLoader with SSR disabled
-const LogoLoader = dynamic(() => import('@/components/loader'), { ssr: false });
 
 const LogoLoaderWrapper: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [showHome, setShowHome] = useState(false);
   const { theme } = useContext(ThemeContext);
 
-  useEffect(() => {
-    console.log("LogoLoaderWrapper mounted");
-    return () => console.log("LogoLoaderWrapper unmounted");
-  }, []);
-
   const handleAnimationComplete = () => {
     setIsLoading(false);
     setShowHome(true);
   };
+
+  useEffect(() => {
+    console.log("LogoLoaderWrapper mounted");
+    return () => console.log("LogoLoaderWrapper unmounted");
+  }, []);
 
   return (
     <>
