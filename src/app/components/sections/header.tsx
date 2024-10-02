33c4c2eaba3f10db5343cb59e4ed0a9cac4ff292
Kieran <kieran.canter@peraton.com>
2024-10-02 19:30:10 -0400

    Fixed logo color

--- src/app/components/sections/header.tsx
+++ src/app/components/sections/header.tsx
@@ -3,7 +3,6 @@ import { ThemeContext } from '@/context/themeContext';
 import LogoSVG from '@/app/components/logoSVG';
 import ThemeSwitcher from '@/app/components/themeSwitcher';
 import Link from 'next/link';
-import Image from 'next/image';
 
 export const Header: React.FC = () => {
   const { theme } = useContext(ThemeContext);
@@ -11,7 +10,9 @@ export const Header: React.FC = () => {
   return (
     <header className="flex flex-row bg-bg/50 justify-between items-center max-w-full h-fit mx-auto px-16 py-6 backdrop-blur-[0.15rem]">
       <div className="relative flex flex-1">
-        <LogoSVG color="fgSoft" hoverColor="fgHard" width="2.5rem" height="2.5rem"/>
+        <Link href="https://kierancanter.dev" className="w-10 h-10 fill-fgSoft hover:fill-fgContrast transition duration-[250ms]">
+          <LogoSVG/>
+        </Link>
       </div>
 
       <div className="relative flex flex-row justify-between items-center w-fit h-fit gap-8">
@@ -21,7 +22,7 @@ export const Header: React.FC = () => {
       </div>
 
       <div id="theme-switcher" className="flex flex-1 flex-row gap-4 items-center justify-end">
-        <p className="relative font-mono transition-colors duration-[250ms] font-bold text-fgContrast selection:bg-fgContrast selection:text-bg">
+        <p className="relative font-ibm-plex-mono transition-colors duration-[250ms] font-bold text-fgContrast selection:bg-fgContrast selection:text-bg">
           {theme}
         </p>
         <ThemeSwitcher />
