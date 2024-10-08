a4cc07b86c6bbf473ffb040c6007e3eefdba98d2
Kieran <kieran.canter@peraton.com>
2024-10-08 19:27:36 -0400

    Added headshot and image effects

--- src/app/components/sections/about.tsx
+++ src/app/components/sections/about.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
+import '@/styles/globals.scss';
 import SectionTitle from '@/app/components/sectionTitle';
 import Image from 'next/image';
 
 const About: React.FC = () => {
 
   return (
-    <div className="flex flex-col w-[calc(100%-4rem)] lg:w-kic-width justify-center items-center">
+    <div className="*:outline flex flex-col w-[calc(100%-4rem)] lg:w-kic-width justify-center items-center">
       <SectionTitle title="About" />
       <div className="relative flex flex-col md:flex-row mt-8 w-full h-fit">
-        <Image 
-          src="/assets/images/logo.svg" 
-          alt="Headshot" 
-          width= {0}
-          height= {0}
-          sizes="100vw"
-          style = {{
-            aspectRatio: "1/1",
-            width: "17rem",
-            height: "auto",
-          }}
-          className="relative flex m-auto md:ml-0 md:mt-0 shadow-black shadow-[0_0.5rem_0.5rem_-0.375rem] hover:shadow-black hover:shadow-[0_0.5rem_0.5rem_-0.125rem] hover:-translate-y-[0.313rem] transition duration-[250ms]" 
-        />  
+        <div className="relative flex flex-[1] w-[17rem] h-[17rem] m-auto md:ml-0 md:mt-0 justify-center items-center transition duration-[250ms] overflow-hidden shadow-black shadow-[0_0.5rem_0.5rem_-0.375rem] hover:shadow-black hover:shadow-[0_0.5rem_0.5rem_-0.125rem] hover:-translate-y-[0.313rem] grayscale-[0.8] hover:grayscale-0 opacity-80 hover:opacity-100">
+          <Image 
+            src="/assets/images/headshot.jpg" 
+            alt="Headshot" 
+            width= {0}
+            height= {0}
+            sizes="100vw"
+            className="w-full h-full relative flex select-none object-cover hover:scale-105 transition duration-[250ms]" 
+          />
+        </div>
+        
 
-        <div id="text-container" className="relative flex flex-col mt-8 md:mt-0 md:ml-8">
+        <div id="text-container" className="relative flex flex-col flex-[2] mt-8 md:mt-0 md:ml-8">
           <p className="relative font-ibm-plex-sans text-md text-fgSoft break-words whitespace-pre-line">
             {`Lorem ipsum odor amet, consectetuer adipiscing elit. Suspendisse vulputate commodo semper torquent aliquam. Pretium molestie donec hendrerit dui tempor commodo lacus. Condimentum taciti nunc mauris phasellus curabitur gravida. Laoreet molestie amet bibendum rhoncus dolor luctus neque id.
 
@@ -31,7 +30,7 @@ const About: React.FC = () => {
             Ex adipiscing diam dignissim accumsan dapibus:`}
           </p>
 
-          <ul className="relative columns-2 font-ibm-plex-mono text-sm text-fgSoft list-disc pl-4 mt-2">
+          <ul className="relative columns-2 font-ibm-plex-mono text-sm text-fgSoft list-inside marker-arrow mt-2 ">
             <li>Python</li>
             <li>HTML + SCSS</li>
             <li>JavaScript/TypeScript</li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   future: {
-    hoverOnlyWhenSupported: true,
+    hoverOnlyWhenSupported: false,
   },
   theme: {
     extend: {
