b9fcd6bdf6b8a8156c7f987689b8dc4046dc24ca
Kieran <kieran.canter@peraton.com>
2024-10-08 19:38:43 -0400

    Consolidated image effects in globals class

--- src/app/components/sections/about.tsx
+++ src/app/components/sections/about.tsx
@@ -6,17 +6,18 @@ import Image from 'next/image';
 const About: React.FC = () => {
 
   return (
-    <div className="*:outline flex flex-col w-[calc(100%-4rem)] lg:w-kic-width justify-center items-center">
+    <div className="flex flex-col w-[calc(100%-4rem)] lg:w-kic-width justify-center items-center">
       <SectionTitle title="About" />
       <div className="relative flex flex-col md:flex-row mt-8 w-full h-fit">
-        <div className="relative flex flex-[1] w-[17rem] h-[17rem] m-auto md:ml-0 md:mt-0 justify-center items-center transition duration-[250ms] overflow-hidden shadow-black shadow-[0_0.5rem_0.5rem_-0.375rem] hover:shadow-black hover:shadow-[0_0.5rem_0.5rem_-0.125rem] hover:-translate-y-[0.313rem] grayscale-[0.8] hover:grayscale-0 opacity-80 hover:opacity-100">
+        
+        <div className="relative flex flex-[1] w-[17rem] h-[17rem] m-auto md:ml-[0.01rem] md:mt-0 justify-center items-center image-effects">
           <Image 
             src="/assets/images/headshot.jpg" 
             alt="Headshot" 
             width= {0}
             height= {0}
             sizes="100vw"
-            className="w-full h-full relative flex select-none object-cover hover:scale-105 transition duration-[250ms]" 
+            className="w-full h-full relative flex select-none object-cover transition duration-[250ms]" 
           />
         </div>
         
