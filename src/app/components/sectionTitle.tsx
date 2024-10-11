2bbdf5dee8b1ab98e821588307150510df2edaef
Kieran <canter.kieran@gmail.com>
2024-10-11 00:26:04 -0400

    Thinned horz. rule, made heading contrast more

--- src/app/components/sectionTitle.tsx
+++ src/app/components/sectionTitle.tsx
@@ -4,11 +4,11 @@ const SectionTitle: React.FC<{ title: string }> = ({ title} ) => {
 
   return (
     <div className="flex flex-row w-full justify-between items-center">
-      <hr className="w-auto flex-1 border-[0.1rem] border-fgSoft" />
-      <h1 className="w-fit text-fgSoft text-[1.5rem] font-ibm-plex-sans font-semibold mx-4">
+      <hr className="w-auto flex-1 border-[0.01rem] border-fgSoft" />
+      <h1 className="w-fit text-fgHard text-[1.5rem] font-ibm-plex-sans font-semibold mx-4">
         {title}
       </h1>
-      <hr className="w-auto flex-1 border-[0.1rem] border-fgSoft" />
+      <hr className="w-auto flex-1 border-[0.05rem] border-fgSoft" />
     </div>
   );
     
