6966df04afdc674a027814c033af6f604f6e2d9f
Kieran <canter.kieran@gmail.com>
2024-10-11 22:14:53 -0400

    Added bullet information for Peraton

--- src/data/experienceContent.ts
+++ src/data/experienceContent.ts
@@ -5,9 +5,10 @@ export const experienceContent = [
     company: 'Peraton',
     period: 'October 2023 — Present',
     bullets: [
-      'Developed feature A',
-      'Led team for project B',
-      'Implemented technology C',
+      'Drove the design and development of a custom clipboard monitoring solution for specific Linux-based systems using <span class="highlight">C</span> and <span class="highlight">JavaScript</span>',
+      'Played a key role in the design and implementation of a streamlined process for building and deploying RPM packages for custom-modified applications, ensuring consistent and scalable distribution across multiple environments using <span class="highlight">Bash</span>',
+      'Contributed to a large-scale codebase using <span class="highlight">Git</span> for version control and collaborative development on new features/enhancements',
+      'Supported Agile project workflow and team collaboration by organizing tasks in <span class="highlight">Jira</span> and documenting processes/technical knowledge in <span class="highlight">Confluence</span>'
     ],
   },
   {
