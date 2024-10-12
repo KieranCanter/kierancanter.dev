d9e118ca126b0906eb3ccf46950081e54e6537e5
Kieran <canter.kieran@gmail.com>
2024-10-12 13:51:21 -0400

    Change 'image-effects' to 'headshot-effects'

--- src/app/components/sections/about.tsx
+++ src/app/components/sections/about.tsx
@@ -11,7 +11,7 @@ const About: React.FC = () => {
       <SectionTitle title="About" />
       <div className="relative flex flex-col md:flex-row mt-8 w-full h-fit justify-center items-center">
         
-        <div className="image-effects relative flex flex-[1] aspect-square min-w-[15rem] w-[17rem] max-w-[19rem] h-auto m-auto md:mt-[0.1rem] md:ml-[0.1rem] justify-center items-center select-none object-cover">
+        <div className="headshot-effects relative flex flex-[1] aspect-square min-w-[15rem] w-[17rem] max-w-[19rem] h-auto m-auto md:mt-[0.1rem] md:ml-[0.1rem] justify-center items-center select-none object-cover">
           <Image 
             src="/assets/images/headshot.jpg" 
             alt="Headshot" 
