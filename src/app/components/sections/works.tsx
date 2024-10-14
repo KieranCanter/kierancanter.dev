c0786040918cbab71025fbe1c2d70db5ec539d52
Kieran <canter.kieran@gmail.com>
2024-10-14 00:13:08 -0400

    Changed links

--- src/app/components/sections/works.tsx
+++ src/app/components/sections/works.tsx
@@ -12,9 +12,9 @@ const Experience: React.FC = () => {
           title="kierancanter.dev" 
           description="Hardware: The parts of a computer system that can be kicked. (Jeff Pesis) I think Microsoft named .Net so it wouldn’t show up in a Unix directory listing.(Oktal)<br><br>Optimism is an occupational hazard of programming; feedback is the treatment. (Kent Beck)<br><br>There are only two industries that refer to their customers as ‘users’. (Edward Tufte)<br><br>There are only two industries that refer to their customers as ‘users’. (Edward Tufte)"
           technologies={["React.js", "Next.js", "TypeScript", "Tailwind CSS", "Node.js"]} 
-          imageUrl="/images/flipready.png"
-          githubUrl="https://github.com/kieran-canter/project1" 
-          projectUrl="https://project1.com" 
+          imageUrl="/images/projectcard-kcdev2.png"
+          githubUrl="https://github.com/KieranCanter/kierancanter.dev" 
+          projectUrl="https://kierancanter.dev"
         />
       </div>
     </div>
