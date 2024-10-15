844ed0666531c7923d2d899f9261b4970c84fd38
Kieran <kieran.canter@peraton.com>
2024-10-15 17:46:58 -0400

    Refined formatting, added circles to display active jobs

--- src/app/experience/experience.tsx
+++ src/app/experience/experience.tsx
@@ -5,19 +5,25 @@ import { experienceContent } from '@/data/experienceContent';
 const Experience: React.FC = () => {
 
   return (
-    <div id="text-container" className="relative flex flex-col gap-4 w-full lg:w-kic-width h-auto m-4 lg:m-auto overflow-y-auto scrollbox pointer-events-none [&_*]:pointer-events-auto">
-      <div className="relative flex flex-col justify-between gap-2 w-full h-fit p-4 bg-black/10 rounded-sm">
-        <h4 className="font-ibm-plex-sans text-base text-fgContrast font-semibold" dangerouslySetInnerHTML={{ __html: experienceContent[0].position}}></h4>
-        <div className="flex flex-row justify-between items-end">
-          <h5 className="text-fgSoft text-sm font-ibm-plex-sans font-light" dangerouslySetInnerHTML={{ __html: experienceContent[0].company}}></h5>
-          <h6 className="text-fgSoft text-xs font-ibm-plex-mono font-semibold" dangerouslySetInnerHTML={{ __html: experienceContent[0].period}}></h6>
+    <div id="text-container" className="relative flex flex-col gap-4 w-full lg:w-kic-width h-fit m-4 lg:m-auto overflow-y-auto scrollbox pointer-events-none [&_*]:pointer-events-auto">
+      <div className="relative flex flex-col justify-between gap-2 w-full p-4 bg-black/10 rounded-sm">
+        <div className="w-full h-fit flex flex-row justify-between items-center">
+          <h4 className="relative w-fit font-ibm-plex-sans text-base lg:text-lg text-fgContrast font-semibold selection:bg-fgContrast" dangerouslySetInnerHTML={{ __html: experienceContent[0].position}}></h4>
+          <div className="relative flex rounded-full w-2.5 h-2.5 lg:w-3 lg:h-3 bg-fgContrast" />
+        </div>
+        <div className="w-full h-fit flex flex-row justify-between items-end">
+          <h5 className="text-fgSoft text-sm lg:text-base font-ibm-plex-sans font-light" dangerouslySetInnerHTML={{ __html: experienceContent[0].company}}></h5>
+          <h6 className="text-fgSoft text-xs lg:text-sm font-ibm-plex-mono font-semibold" dangerouslySetInnerHTML={{ __html: experienceContent[0].period}}></h6>
         </div>
       </div>
-      <div className="relative flex flex-col justify-between gap-2 w-full h-fit p-4 bg-black/10 rounded-sm">
-      <h4 className="font-ibm-plex-sans text-base text-fgContrast font-semibold" dangerouslySetInnerHTML={{ __html: experienceContent[1].position}}></h4>
-        <div className="flex flex-row justify-between items-end">
-          <h5 className="text-fgSoft text-sm font-ibm-plex-sans font-light" dangerouslySetInnerHTML={{ __html: experienceContent[1].company}}></h5>
-          <h6 className="text-fgSoft text-xs font-ibm-plex-mono font-semibold" dangerouslySetInnerHTML={{ __html: experienceContent[1].period}}></h6>
+      <div className="relative flex flex-col justify-between gap-2 w-full p-4 bg-black/10 rounded-sm">
+        <div className="w-full h-fit flex flex-row justify-between items-center">
+          <h4 className="relative w-fit font-ibm-plex-sans text-base lg:text-lg text-fgContrast font-semibold selection:bg-fgContrast" dangerouslySetInnerHTML={{ __html: experienceContent[1].position}}></h4>
+          <div className="relative flex rounded-full w-2.5 h-2.5 lg:w-3 lg:h-3 bg-fgContrast" />
+        </div>
+        <div className="w-full h-fit flex flex-row justify-between items-end">
+          <h5 className="text-fgSoft text-sm lg:text-base font-ibm-plex-sans font-light" dangerouslySetInnerHTML={{ __html: experienceContent[1].company}}></h5>
+          <h6 className="text-fgSoft text-xs lg:text-sm font-ibm-plex-mono font-semibold" dangerouslySetInnerHTML={{ __html: experienceContent[1].period}}></h6>
         </div>
       </div>
     </div>
