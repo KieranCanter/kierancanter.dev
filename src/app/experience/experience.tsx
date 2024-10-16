2d7519124333297d977b4df72c8bb21b11b94ba9
Kieran <canter.kieran@gmail.com>
2024-10-16 13:20:20 -0400

    Experience content maps to elements

--- src/app/experience/experience.tsx
+++ src/app/experience/experience.tsx
@@ -3,35 +3,25 @@ import '@/styles/globals.scss';
 import { experienceContent } from '@/data/experienceContent';
 
 const Experience: React.FC = () => {
-
   return (
-    <div id="text-container" className="relative flex flex-col gap-4 w-full lg:w-kic-width h-fit overflow-y-auto scrollbox pointer-events-none [&_*]:pointer-events-auto">
-      <div className="relative flex flex-col justify-between gap-2 w-full p-4 bg-black/10 rounded-sm transition-colors duration-[250ms] lg:hover:bg-black/20">
-        
-        <div className="w-full h-fit flex flex-row justify-between items-center">
-          <h4 className="relative w-fit font-ibm-plex-sans text-base lg:text-lg text-fgContrast font-semibold selection:bg-fgContrast" dangerouslySetInnerHTML={{ __html: experienceContent[0].position}}></h4>
-          <div className="relative flex rounded-full w-2.5 h-2.5 lg:w-3 lg:h-3 bg-fgContrast" />
-        </div>
-        <div className="w-full h-fit flex flex-row justify-between items-end">
-          <h5 className="text-fgSoft text-sm lg:text-base font-ibm-plex-sans font-light" dangerouslySetInnerHTML={{ __html: experienceContent[0].company}}></h5>
-          <h6 className="text-fgSoft text-xs lg:text-sm font-ibm-plex-mono font-semibold" dangerouslySetInnerHTML={{ __html: experienceContent[0].period}}></h6>
-        </div>
+    <div id="text-container" className="relative flex flex-col gap-4 w-full lg:w-kic-width h-fit overflow-y-auto pointer-events-none [&_*]:pointer-events-auto">
+      {experienceContent.map((experience, index) => (
+        <div key={index} className="relative flex flex-col justify-between gap-2 w-full p-4 bg-black/10 rounded-sm transition-colors duration-[250ms] lg:hover:bg-black/20">
+          
+          <div className="w-full h-fit flex flex-row justify-between items-center">
+            <h4 className="relative w-fit font-ibm-plex-sans text-base lg:text-lg text-fgContrast font-semibold selection:bg-fgContrast" dangerouslySetInnerHTML={{ __html: experience.position }}></h4>
+            <div className="relative flex rounded-full w-2.5 h-2.5 lg:w-3 lg:h-3 bg-fgContrast" />
+          </div>
+          
+          <div className="w-full h-fit flex flex-row justify-between items-end">
+            <h5 className="text-fgSoft text-sm lg:text-base font-ibm-plex-sans font-light" dangerouslySetInnerHTML={{ __html: experience.company }}></h5>
+            <h6 className="text-fgSoft text-xs lg:text-sm font-ibm-plex-mono font-semibold" dangerouslySetInnerHTML={{ __html: experience.period }}></h6>
+          </div>
 
-      </div>
-      <div className="relative flex flex-col justify-between gap-2 w-full p-4 bg-black/10 rounded-sm transition-colors duration-[250ms] lg:hover:bg-black/20">
-        <div className="w-full h-fit flex flex-row justify-between items-center">
-          <h4 className="relative w-fit font-ibm-plex-sans text-base lg:text-lg text-fgContrast font-semibold selection:bg-fgContrast" dangerouslySetInnerHTML={{ __html: experienceContent[1].position}}></h4>
-          <div className="relative flex rounded-full w-2.5 h-2.5 lg:w-3 lg:h-3 bg-fgContrast" />
         </div>
-        <div className="w-full h-fit flex flex-row justify-between items-end">
-          <h5 className="text-fgSoft text-sm lg:text-base font-ibm-plex-sans font-light" dangerouslySetInnerHTML={{ __html: experienceContent[1].company}}></h5>
-          <h6 className="text-fgSoft text-xs lg:text-sm font-ibm-plex-mono font-semibold" dangerouslySetInnerHTML={{ __html: experienceContent[1].period}}></h6>
-        </div>
-        
-      </div>
+      ))}
     </div>
   );
-    
 };
 
 export default Experience;
--- src/data/experienceContent.ts
+++ src/data/experienceContent.ts
@@ -1,26 +1,13 @@
 export const experienceContent = [
   {
-    id: 'tab1',
     position: 'Cyber Software Engineer',
     company: 'Peraton Inc.',
     period: 'Oct. 2023 — Present',
-    bullets: [
-      'Drove the design and development of a custom clipboard monitoring solution for specific Linux-based systems using <span class="highlight">C</span> and <span class="highlight">JavaScript</span>',
-      'Played a key role in the design and implementation of a streamlined process for building and deploying RPM packages for custom-modified applications, ensuring consistent and scalable distribution across multiple environments using <span class="highlight">Bash</span>',
-      'Contributed to a large-scale codebase using <span class="highlight">Git</span> for version control and collaborative development on new features/enhancements',
-      'Supported <span class="highlight">Agile</span> project workflow and team collaboration by organizing tasks in <span class="highlight">Jira</span> and documenting processes/technical knowledge in <span class="highlight">Confluence</span>'
-    ],
   },
   {
-    id: 'tab2',
     position: 'Petroleum Supply Specialist (92F)',
     company: 'U.S. Army Reserve',
     period: 'Mar. 2019 — Present',
-    bullets: [
-      'As Platoon Guide, <span class="highlight">led a 50-man unit through high-pressure challenges</span> including ruck marches, emergency field care & aid, and patrol base establishment',
-      '<span class="highlight">Led small teams (≤7 personnel)</span> in conducting fuel operations, coordinating the issue and receipt of over 20,000 gallons of diesel fuel with precision',
-      '<span class="highlight">Conducted presentations and taught classes to medium-sized groups of soldiers</span>, communicating complex procedures in an accessible fashion',
-    ],
   },
   
 ];
