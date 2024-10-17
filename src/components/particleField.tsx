cbf2f2f5aee2b210afaf744c0bc74f0425b07ef1
Kieran <kieran.canter@peraton.com>
2024-10-17 15:53:07 -0400

    Fixed inset on particle field bounds

--- src/components/particleField.tsx
+++ src/components/particleField.tsx
@@ -220,7 +220,7 @@ const ParticleField: React.FC<ParticleFieldProps> = ({ color }) => {
   }, [color]);
 
   return (
-    <div ref={containerRef} className="absolute inset-4 lg:inset-8">
+    <div ref={containerRef} className="absolute inset-0">
       <canvas 
         ref={canvasRef} 
         className="w-full h-full" 
