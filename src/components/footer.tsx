bac14cd19ac0c4b4403abea57ff3edc306bae60a
Kieran <canter.kieran@gmail.com>
2024-10-16 21:49:25 -0400

    Made footer bolder

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <Link href="https://github.com/kierancanter/kierancanter.dev" target="_blank" rel="noopener noreferrer">
-      <p className="text-center font-ibm-plex-serif text-fgSoft text-[10px] hover:text-fgContrast transition-colors duration-[250ms]">with passion by <span className="font-spectral-sc">Kieran CANTER</span> — {new Date().getFullYear()}</p>
+      <p className="text-center font-ibm-plex-serif text-fgSoft text-[10px] font-medium hover:text-fgContrast transition-colors duration-[250ms]">with passion by <span className="font-spectral-sc font-semibold">Kieran CANTER</span> — {new Date().getFullYear()}</p>
     </Link>
   );
 };
