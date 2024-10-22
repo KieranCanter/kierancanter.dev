bdfe5bcfc8a8da3f77e3dac030cc9c6b6cb3c631
Kieran <canter.kieran@gmail.com>
2024-10-22 19:33:58 -0400

    Incorporated svg as bullet to fix mobile bullet color issues

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.scss';
 import { ThemeContext } from '@/context/themeContext';
 import { generateAccentColor } from '@/util/colorfulSetter';
 import { aboutContent } from '@/data/aboutContent';
+import DiamondBullet from '@/components/diamondBullet';
 
 const About: React.FC = () => {
   const { theme } = useContext(ThemeContext);
@@ -12,7 +13,7 @@ const About: React.FC = () => {
       id="text-container" 
       className="relative flex flex-col w-full lg:w-kic-width h-fit p-4 text-fgSoft bg-black/10 rounded-sm transition-colors duration-[250ms] overflow-y-auto lg:hover:bg-black/20">
       <p dangerouslySetInnerHTML={{ __html: aboutContent.bio }}></p>
-      <ul className="relative columns-2 mt-2 font-ibm-plex-mono text-xs md:text-sm list-outside">
+      <ul className="relative columns-2 mt-2 font-ibm-plex-mono text-xs md:text-sm list-none">
         {aboutContent.skills.map((skill: string, index: number) => {
           let accentColor: string;
           if (theme === 'brilliant' || theme === 'luminous') {
@@ -22,22 +23,15 @@ const About: React.FC = () => {
           }
 
           return (
-            <li key={index} className="mb-1 break-words text-fgHard selection:bg-fgHard">
-              <span
-                style={{ color: accentColor }}
-                className="mr-1"
-              >
-                ♦ 
-              </span>
+            <li key={index} className="mb-1 break-words text-fgHard selection:bg-fgHard flex items-center">
+              <DiamondBullet color={accentColor} />
               {skill}
             </li>
           );
-          
         })}
       </ul>
     </div>
   );
-    
 };
 
 export default About;
