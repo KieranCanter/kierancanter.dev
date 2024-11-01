6ee4b0204a15896ec38350743eb50524ec44d577
Kieran <kieran.canter@peraton.com>
2024-11-01 13:21:05 -0400

    Allowed bg-opacity change on hover for all screen sizes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,7 @@ export default function RootLayout({
           
           <div className="relative h-[100dvh] md:h-[100vh] bg-bg text-fgSoft p-4 lg:p-8 transition-colors duration-[250ms] overflow-clip">
             <div className="relative flex flex-col h-full justify-between border border-fgHard min-h-[calc(100dvh-2rem)] md:min-h-[calc(100dvh-4rem)] transition-colors duration-[250ms]">
+              
               <ParticleField color="rgb(110, 110, 110)" />
               <header className="flex relative w-full md:w-[calc(100%-2rem)] lg:w-[calc(100%-4rem)] h-fit my-4 lg:mt-8 lg:mb-16 mx-auto justify-between pointer-events-none">
                 <div className="relative flex mx-auto md:max-lg:ml-0 lg:justify-center">
