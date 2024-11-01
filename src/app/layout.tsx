fac1320985ffa185b29c3f760e0090125c2c71d3
Kieran <canter.kieran@gmail.com>
2024-11-01 16:35:38 -0400

    Wrapped content in navigation component to handle swiping

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@/context/themeContext';
 import ParticleField from "@/components/particleField";
 import Header from "@/components/header";
 import ThemeSwitcher from "@/components/themeSwitcher";
+import Navigation from "@/app/navigation";
 import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
@@ -83,33 +84,37 @@ export default function RootLayout({
       </head>
       <body className="antialiased bg-bg text-fgSoft selection:text-bg selection:bg-fgSoft">
         <ThemeProvider>
-          <a href="#main-content" className="sr-only focus:not-sr-only">Skip to main content</a>
-          
-          <div className="relative h-[100dvh] md:h-[100vh] bg-bg text-fgSoft p-4 lg:p-8 transition-colors duration-[250ms] overflow-clip">
-            <div className="relative flex flex-col h-full justify-between border border-fgHard min-h-[calc(100dvh-2rem)] md:min-h-[calc(100dvh-4rem)] transition-colors duration-[250ms]">
-              
-              <ParticleField color="rgb(110, 110, 110)" />
-              <header className="flex relative w-full md:w-[calc(100%-2rem)] lg:w-[calc(100%-4rem)] h-fit my-4 lg:mt-8 lg:mb-16 mx-auto justify-between pointer-events-none">
-                <div className="relative flex mx-auto md:max-lg:ml-0 lg:justify-center">
-                  <Header />
-                </div>
-                <div className="max-md:hidden relative flex justify-end lg:absolute lg:top-0 lg:right-0 lg:m-0 pointer-events-auto  ">
+
+          <Navigation>
+            
+            <a href="#main-content" className="sr-only focus:not-sr-only">Skip to main content</a>
+            
+            <div className="relative h-[100dvh] md:h-[100vh] bg-bg text-fgSoft p-4 lg:p-8 transition-colors duration-[250ms] overflow-clip">
+              <div className="relative flex flex-col h-full justify-between border border-fgHard min-h-[calc(100dvh-2rem)] md:min-h-[calc(100dvh-4rem)] transition-colors duration-[250ms]">
+                
+                <ParticleField color="rgb(110, 110, 110)" />
+                <header className="flex relative w-full md:w-[calc(100%-2rem)] lg:w-[calc(100%-4rem)] h-fit my-4 lg:mt-8 lg:mb-16 mx-auto justify-between pointer-events-none">
+                  <div className="relative flex mx-auto md:max-lg:ml-0 lg:justify-center">
+                    <Header />
+                  </div>
+                  <div className="max-md:hidden relative flex justify-end lg:absolute lg:top-0 lg:right-0 lg:m-0 pointer-events-auto  ">
+                    <ThemeSwitcher />
+                  </div>
+                </header>
+                
+                {children}
+
+                <div className="md:hidden relative flex justify-end m-4 pointer-events-auto">
                   <ThemeSwitcher />
                 </div>
-              </header>
-              
-              {children}
-
-              <div className="md:hidden relative flex justify-end m-4 pointer-events-auto">
-                <ThemeSwitcher />
               </div>
+              
+              <footer className="relative w-fit h-fit m-auto ml-0 flex">
+                <Footer />
+              </footer>
             </div>
-            
-            <footer className="relative w-fit h-fit m-auto ml-0 flex">
-              <Footer />
-            </footer>
-          </div>
 
+          </Navigation>
         </ThemeProvider>
       </body>
     </html>
