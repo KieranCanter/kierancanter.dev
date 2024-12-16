b12153b580cd634e8f45fd2d30f763c7ca90526c
Kieran <canter.kieran@gmail.com>
2024-12-16 13:00:31 -0500

    Removed unused analytics util

--- src/util/analytics.ts
+++
@@ -1,9 +0,0 @@
-type TrackEvent = {
-  track: (eventName: string, eventData?: object) => void;
-}
-  
-export const trackEvent = (eventName: string, eventData?: object) => {
-  if (typeof window !== 'undefined' && (window as any).umami) {
-    (window as any).umami.track(eventName, eventData);
-  }
-};
