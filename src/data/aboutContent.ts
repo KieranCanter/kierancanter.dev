a497fd0b57f61d5ef72a15bf5d2ca4331d16de89
Kieran <canter.kieran@gmail.com>
2024-12-16 16:56:21 -0500

    Updated favorite technologies

--- src/data/aboutContent.ts
+++ src/data/aboutContent.ts
@@ -8,12 +8,14 @@ export const aboutContent = {
   bio: "I'm Kieran, a computer scientist with a passion for not only designing, developing, and deploying software, but most importantly, <span class='highlight'>impressing</span>. I prefer to call myself a computer scientist because of my deep enthusiasm for various aspects of the discipline like software engineering, web development, data science, algorithms & data structures, object-oriented programming, and theoretical mathematics. More than anything, I value my teachability and aptitude for quickly learning new concepts.<br><br>Initially, my interest in computers stemmed from a friend showing me little programs he would write on his TI-84 calculator. This sparked a curiosity in me which led to custom PC build videos populating my YouTube feed. Hooked on hardware, I researched and built my own computer, obsessed with consumer technology. Naturally, I headed off to college in pursuit of a Computer Engineering degree... until I discovered how disinterested I was in the electrical engineering side of the field. I fell in love with the programming and math involved in software and expeditiously switched majors, finishing up at the University of Maryland with a B.S. in Computer Science.<br><br>Nowadays, I'm a <span class='highlight'>Software Engineer</span> who happens to enjoy <span class='highlight'>weightlifting</span>, <span class='highlight'>MMA</span>, <span class='highlight'>philosophy</span>, <span class='highlight'>financial literacy</span>, and of course, <span class='highlight'>coding for fun</span>.<br><br>Some of my favorite technologies:",
   skills: [
     'Python',
-    'C/C++',
     'Java',
     'TypeScript',
-    'HTML5 + CSS3',
-    'React.js',
+    'Kotlin',
+    'Ruby',
+    'PostgreSQL',
+    'React',
     'Next.js',
-    'Tailwind CSS'
+    'Pandas',
+    'scikit-learn'
   ]
 };
